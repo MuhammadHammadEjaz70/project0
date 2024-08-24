58d146043ae8d6b611507168a12a3d46bdd3788b
Omar Ahmed <devomar323@gmail.com>
2024-08-24 17:42:31 +0500

    DB connection method 2

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import express from 'express';
+// require('dotenv').config({path:'./env'})
+// import dotenv from 'dotenv';
+// dotenv.config({
+//     path:"./env"
+// })
 
-const app= express();
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error",(err)=>{
-            console.log("Error: App is not able to talk to DB",err)
-            throw err;
-        })
-        app.listen(process.env.PORT,()=>{
-            console.log(`Backend is listening on port ${process.env.PORT}`)
-        })
-    } catch (error) {
-        console.error("ERROR", error)
-        throw err;
-    }
+// *****
+// node v20.06+ there is no need for dotenv package as node provided built-in support for .env files
 
-})()
+import connectDB from "./db/index.js";
+connectDB();
+
+
+
+
+
+
+
+
+
+
+// /*
+// const app= express();
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(err)=>{
+//             console.log("Error: App is not able to talk to DB",err)
+//             throw err;
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Backend is listening on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("ERROR", error)
+//         throw err;
+//     }
+
+// })()*/
