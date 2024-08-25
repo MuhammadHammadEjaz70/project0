947dc5e625f547a0fa9e73285154437df71fc374
Omar Ahmed <devomar323@gmail.com>
2024-08-25 16:05:54 +0500

    user and video model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,5 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-    }
+    (req, res, next) => Promise.resolve(requestHandler(req, res, next)).catch(next)
 }
 
 export { asyncHandler }
