de7d93e1b83d26c6248f09709e028f96ebf384a9
Omar Ahmed <devomar323@gmail.com>
2024-08-26 15:52:29 +0500

    multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,55 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs'
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUNDINARY_CLOUD_NAME,
+    api_key: CLOUNDINARY_API_KEY,
+    api_secret: CLOUNDINARY_API_SECERT
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // upload the file
+        const res = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        //file has been uploaded successfully
+        console.log("File is uploaded on cloudinary ", res.url);
+        console.log("File is uploaded on cloudinary ", res);
+        return res;
+
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) 
+        //remove the locally saved temporariy file as the upload operation got failed
+        console.log(error); 
+        return null;  
+    }
+}
+
+
+export {uploadOnCloudinary}
+
+// (async function () {
+
+//     // Optimize delivery by resizing and applying auto-format and auto-quality
+//     const optimizeUrl = cloudinary.url('shoes', {
+//         fetch_format: 'auto',
+//         quality: 'auto'
+//     });
+
+//     console.log(optimizeUrl);
+
+//     // Transform the image: auto-crop to square aspect_ratio
+//     const autoCropUrl = cloudinary.url('shoes', {
+//         crop: 'auto',
+//         gravity: 'auto',
+//         width: 500,
+//         height: 500,
+//     });
+
+//     console.log(autoCropUrl);
+// })();
